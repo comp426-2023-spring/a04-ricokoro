a0cbe461d68b2b7cd821203e45c7d06ec1a4fedf
ricokoro <77748744+ricokoro@users.noreply.github.com>
2023-05-09 14:38:03 -0400

    Update server.js

--- .github/server.js
+++ .github/server.js
@@ -1,57 +1,58 @@
-import minimist from 'minimist'
-import express from "express"
-import { rps, rpsls } from "../lib/rpsls.js";
+#!/usr/bin/env node
 
-const args = minimist(process.argv.slice(2));
-const app = express();
-const port = args["port"] || 5000
+import { rps, rpsls } from "./lib/rpsls.js"
+import minimist from "minimist"
+import express from "express"
+
+var argv = minimist(process.argv.slice(2));
+var app = express()
+const port = argv.port || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get("/app/", (req, res) => {
-    res.status(200).send('200 OK');
+app.get('/app/', (req, res) => {  
+    res.status(200).send("200 OK")
 })
 
-app.get("/app/rps/", (req, res) => {
-    res.status(200).send(JSON.stringify(rps()));
+app.get('/app/rps/', (req, res) => { 
+    res.status(200).send(rps(null));
 })
 
-app.get("/app/rpsls/", (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls()));
+app.get('/app/rpsls/', (req, res) => {
+    res.status(200).send(rpsls(null));
 })
 
-app.get("/app/rps/play/", (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.query.shot)));
+app.get('/app/rps/play/', (req, res) => { 
+    res.status(200).send(rps(req.query.shot));
 })
 
-app.get("/app/rpsls/play/", (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
+app.post('/app/rps/play/', (req, res) => { 
+    res.status(200).send(rps(req.body.shot));
 })
 
-app.post("/app/rps/play/", (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.body.shot)));
+app.get('/app/rpsls/play/', (req, res) => { 
+    res.status(200).send(rpsls(req.query.shot));
 })
 
-app.post("/app/rpsls/play/", (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+app.post('/app/rpsls/play/', (req, res) => {
+    res.status(200).send(rpsls(req.body.shot));
 })
 
-app.get('/app/rps/play/:arg', (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.params.arg)));
+app.get('/app/rps/play/:shot/', (req, res) => {
+    res.status(200).send(rps(req.params.shot));
 })
 
-app.get('/app/rpsls/play/:arg', (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.params.arg)));
+app.get('/app/rpsls/play/:shot/', (req, res) => {
+    res.status(200).send(rpsls(req.params.shot));
 })
 
-app.get("*", (req, res) => {
-    res.status(404).send("404 NOT FOUND");
+app.get('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND')
 })
 
-// Start server and listen on specified port
-app.listen(port, () =>{
-    console.log('Server listening on port ' + port);
-})
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
 
 
